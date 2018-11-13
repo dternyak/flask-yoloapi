388eda021418ee7b1c2026de38326a240ed29083
Daniel Ternyak <dternyak@gmail.com>
2018-11-13 18:42:38 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-version = '0.2.5'
+version = '0.2.6'
 README = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(README).read()
 setup(
